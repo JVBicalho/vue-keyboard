4bcb1230e4f7f73a76b23689c0ced5407edf612f
JV Bicalho <contatojvbicalho@gmail.com>
2022-10-31 18:28:30 -0300

    Responsividade Pronta

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,61 @@ import Key from './components/Key.vue'
 </script>
 
 <template>
-  
-    <Key></Key>
+  <main>
+    <div class="keyboard">
+    <Key v-for="items in KeySimbolsArray" :keySymbol="items"></Key>
+  </div>
+  </main>
+ 
+    
+   
   
 </template>
+<script>
+  export default {
+  data() {
+    return {
+      KeySimbolsArray: ["Esc","1","2","3","4","5","6","7","8","9","0","-","=","bksp",
+                        "Tab","q","w","e","r","t","y","u","i","o","p","[","]","\\",
+                        "CapsLK","a","s","d","f","g","h","j","k","l",";","\'","Enter",
+                        "Shift","z","x","c","v","b","n","m","\,","\.","\/","Shift",
+                        "Ctrl","win","alt","space","alt","Fn","opitions","Crtl",
+                      ],
+      KeyTest: ["Q","W",],
+    }
+  },
+}
+      
+    
 
+</script>
 <style scoped>
+
+* {
+  padding: 0;
+  margin: 0;
+  border: 0;
+  box-sizing: border-box
+}
+#app{
+  
+}
+body{
+  background-color:#fff !important;
+  
+}
+main{
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  
+}
+.keyboard{
+
+  width:90vw;
+  flex-direction: row;
+  justify-content: center;
+
+}
 
 </style>
--- src/components/Key.vue
+++ src/components/Key.vue
@@ -1,13 +1,13 @@
 <template >
-    <div>
-        <span><i>Q</i></span>
-    </div>
+    
+        <span><i>{{ keySymbol }}</i></span>
+    
 </template>
 <script>
 export default {
     name : 'Key',
     props : {
-      key : String,
+      keySymbol : String,
     }
 }
 </script>
@@ -15,8 +15,9 @@ export default {
   span{
     position:relative;
     display:inline-block;
-    width: 80px;
-    height:80px;
+    width: 5.4vw;
+    height:8vw;
+    margin-right: 0.6vw !important;
     border-radius:10px;
     background: linear-gradient(180deg, #282828,#202020);
     box-shadow:inset -8px 0 8px rgba(0,0,0,0.15),
@@ -27,11 +28,63 @@ export default {
     padding:8px 15px;
     margin:8px 4px;
   }
+/* Backspace */
+span:nth-child(14) {
+  width: 10.8vw;
+}
+/* Tab */
+span:nth-child(15) {
+  width: 8.1vw;
+}
+/* \ */
+span:nth-child(28) {
+  width: 8.1vw;
+}
+/* Capslock */
+span:nth-child(29) {
+  width: 11vw;
+}
+/* Enter */
+span:nth-child(41) {
+  width: 11.6vw;
+}
+/* Shift */
+span:nth-child(42) {
+  width: 13.6vw;
+}
+span:nth-child(53) {
+  width: 15vw;
+}
+span:nth-child(54) {
+  width: 7.3vw;
+}
+span:nth-child(55) {
+  width: 7.3vw;
+}
+span:nth-child(56) {
+  width: 7.3vw;
+}
+span:nth-child(57) {
+  width: 33.8vw;
+}
+span:nth-child(58) {
+  width: 7.3vw;
+}
+span:nth-child(59) {
+  width: 7.3vw;
+}
+span:nth-child(60) {
+  width: 7.3vw;
+}
+span:nth-child(61) {
+  width: 7.3vw;
+}
+
   span::before{
     content: '';
     position:absolute;
-    top:3px;
-    left:4px;
+    top:4%;
+    left:5%;
     bottom: 14px;
     right: 12px;
     background: linear-gradient(90deg, #232323,#4a4a4a);
@@ -44,9 +97,12 @@ export default {
     border-top : 1px solid #0004; 
   }
   span i{
+    padding-top: 30%;
+    padding-left: 10%;
     position: relative;
     color:#fff;
-    font-size:1.5em;
+    
+    font-size:clamp(0.01em, 2vw ,33.5em);
     font-style:normal;
     text-transform:uppercase;
     
