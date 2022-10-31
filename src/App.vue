42c3c15dfd722c8f5b931dcf6beb607e1c6ee465
JV Bicalho <contatojvbicalho@gmail.com>
2022-10-31 12:48:03 -0300

    Primeiro Commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Key from './components/Key.vue'
+</script>
+
+<template>
+  
+    <Key></Key>
+  
+</template>
+
+<style scoped>
+
+</style>
